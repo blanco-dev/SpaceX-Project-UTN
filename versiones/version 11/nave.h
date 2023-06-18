b18d8bad592820634248c81c87e2d4f12187b2ca
Fabri-dev <fabri.vogol@gmail.com>
2023-06-18 17:27:07 -0300

    Version 11

---
+++ versiones/version 11/nave.h
@@ -0,0 +1,40 @@
+#ifndef NAVES_H_INCLUDED
+#define NAVES_H_INCLUDED
+#define DIM_MAX_ARCHI 15
+#define DIM_MAX_STR 20
+
+typedef struct{
+int id;
+char tipoDeNave[DIM_MAX_STR]; //(Starship, Falcon 9, Falcon Heavy) sólo pueden ser esas tres naves, ningún otro tipo se acepta.
+int CantidadDeVuelosRealizados;
+int HorasDeVueloAcumuladas;
+int estado; // (0- mantenimiento 1- Lista para su uso 2- Actualmente en misión 3- De baja)
+}stNaves;
+
+// validar
+void validarTipoNave(char archivoNaves[]);
+
+// Cargar nave
+stNaves cargarUnaNave(char archivoNaves[]);
+int encontrarUltimaIDNave(char archivoNaves[]);
+void tiposDeNave();
+
+// arreglo
+int cargarArregloNave(stNaves arrNave[], char archivoNaves[]);
+
+// mostrar
+void mostrarUnaNave(stNaves aux);
+void mostrarTodasLasNaves(char archivoNaves);
+void mostrarNavesXEstado(char archivoNaves, int estado);
+
+// archivos
+void arregloNavesToArchivoNaves(char archivoNaves[],stNaves arrNaves[], int validos);
+
+// modificar ?
+
+// consulta
+
+#endif // NAVES_H_INCLUDED
+
+
+
