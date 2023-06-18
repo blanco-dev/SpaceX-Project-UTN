3ecc89c4b4f904b586c2f57e60cb3ea5b4a17510
Matias <blancom-computacion@iepm.com.ar>
2023-06-18 11:17:07 -0300

    version 9 proyect

---
+++ menu.h
@@ -0,0 +1,17 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+#define DIM_MAX_ARCHI 15
+#define DIM_MAX_STR 20
+
+//menu
+void menuPrincipal(char archivoAstronautas[],char archivoNaves[],char archivoMisiones[]);
+int validarOpcionMenu(int dato);
+void pantallaDeCarga();
+void menuAstronauta(char archivoAstronauta[]);
+void opcionMostrarAstronautas(char archivoAstro[]);
+void dibujoAstronauta();
+
+
+
+
+#endif // MENU_H_INCLUDED
