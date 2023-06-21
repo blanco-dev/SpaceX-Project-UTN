d9d46b951395481191acb84c2d98aa860695a7bd
Matias <blancom-computacion@iepm.com.ar>
2023-06-20 23:00:53 -0300

    ascii files

---
+++ versiones/ascii/ascii/main.c
@@ -0,0 +1,213 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void dibujoLuna();
+void dibujoSaturno();
+void dibujoMarte();
+void dibujoEEI();
+void dibujoOrbitaTerrestre();
+
+int main() {
+
+    dibujoLuna();
+    dibujoSaturno();
+    dibujoMarte();
+    dibujoEEI();
+    dibujoOrbitaTerrestre();
+
+    return 0;
+}
+
+void dibujoMarte() {
+
+    puts("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNWWNWWWWWWWWWWWWWWWWWWWWWWNWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNXK0kddollccccllodxkKXNWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNX0xol:,'............ ...':okKNWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNKkdl::;;;;,,'''''.'.......     .;d0NWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWXOdc::lddlcc:;,,,;,,,,''.......      .:kXWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWWWWWWWXOdlccccoxdllc:;;;;:;;;;,,.........       ,xXWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWWWWWN0dlcccclloollc:::;;;;,,'''''.........        ;ONWWWWWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWWWWXkollclclloollcc::::;;,,,'''''''.......         .oXWWWWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWWWXkollllcclolllcccccc:cc:;;;,,,,,,''.......         :KWWWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWWXkoloolccllllllllllllllc:;;;;;;;;,,,'.......         :KWWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWNOolooolllollllllllllllc:;,,,,;;;;;,,,''......         lXWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWW0o::llccoollllccllccccc:;;;,,,;;;;,,''''.......        .xNWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWXklc:c:;:lllcclc::c:;;;;;;;;;;;;:::;,,,,''......         :KWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWKdccccc::lllllll:::::;,,,;;;;;;;:cc:;;;,,'......         .kWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWOocloolccclllolc:;;::;,,,,,,,,,;;:c:;;,,'......          .dWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWNOoldddoolllooolclll:;;;;;;,;;,,'',::;,,''......           oNWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWN0ooddooolloollcclooc;;;;;;;;;;,'.',,,,,''......          .dWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWKxddddllooolc:;;:clc:;;;;:;;;,,'....''..........         .kWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWXkddollllodllc;,;::;:::::;;;,,,'''..............         :KWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWW0xoocccclolllc::::;;::;;;;;,'''''''............        .xWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWNOooollcllllc:clccc:;,;;,;;,'.....''','........        cXWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWWXxlllooollccllllc:::;;;,;;,'.......','.........      :KWWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWWWKxlllllllllolllc;;;;;;;;;;,,'.....''..........     cKWWWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWWWWXOdoolccloolccc::::::::cccc:,,'''''''''''....   .oXWWWWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWWWWWNKkollccccccccc:ccc:;;:;;;,,,;;;;,,,,''....   ;kNWWWWWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWWWWWWWX0dcc::;;;:c::clcc:;,,,,,,,,,;;;,,'....   ,xXWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWXOdc;,,,,;;;:ccc::;;;;,,,,''''....   .:xXWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNKkoc;,,;;;;,,'''''''........  ..:d0NWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNXOkdl:;,'...............,:okKNWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNKOkxdoolllllllodxOKXWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+    puts("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+    puts("NWWNWWWWWWWWWWWWNWWWWWWWWWNWWNWWWWWWNWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNWWWWWWWWWWWWNWWWWWWW");
+}
+
+void dibujoSaturno () {
+
+    puts("                                                                    ..;===+.");
+    puts("                                                                .:=iiiiii=+=");
+    puts("                                                             .=i))=;::+)i=+,");
+    puts("                                                          ,=i);)I)))I):=i=;");
+    puts("                                                       .=i==))))ii)))I:i++");
+    puts("                                                     +)+))iiiiiiii))I=i+:'");
+    puts("                                .,:;;++++++;:,.       )iii+:::;iii))+i='");
+    puts("                             .:;++=iiiiiiiiii=++;.    =::,,,:::=i));=+'");
+    puts("                           ,;+==ii)))))))))))ii==+;,      ,,,:=i))+=:");
+    puts("                         ,;+=ii))))))IIIIII))))ii===;.    ,,:=i)=i+");
+    puts("                        ;+=ii)))IIIIITIIIIII))))iiii=+,   ,:=));=,");
+    puts("                      ,+=i))IIIIIITTTTTITIIIIII)))I)i=+,,:+i)=i+");
+    puts("                     ,+i))IIIIIITTTTTTTTTTTTI))IIII))i=::i))i='");
+    puts("                    ,=i))IIIIITLLTTTTTTTTTTIITTTTIII)+;+i)+i`");
+    puts("                   =i))IIITTLTLTTTTTTTTTIITTLLTTTII+:i)ii:'");
+    puts("                   +i))IITTTLLLTTTTTTTTTTTTLLLTTTT+:i)))=,");
+    puts("                   =))ITTTTTTTTTTTLTTTTTTLLLLLLTi:=)IIiii;");
+    puts("                  .i)IIITTTTTTTTLTTTITLLLLLLLT);=)I)))))i;");
+    puts("                  :))IIITTTTTLTTTTTTLLHLLLLL);=)II)IIIIi=:");
+    puts("                  :i)IIITTTTTTTTTLLLHLLHLL)+=)II)ITTTI)i=");
+    puts("                  .i)IIITTTTITTLLLHHLLLL);=)II)ITTTTII)i+");
+    puts("                  =i)IIIIIITTLLLLLLHLL=:i)II)TTTTTTIII)i'");
+    puts("                +i)i)))IITTLLLLLLLLT=:i)II)TTTTLTTIII)i;");
+    puts("              +ii)i:)IITTLLTLLLLT=;+i)I)ITTTTLTTTII))i;");
+    puts("             =;)i=:,=)ITTTTLTTI=:i))I)TTTLLLTTTTTII)i;");
+    puts("           +i)ii::,  +)IIITI+:+i)I))TTTTLLTTTTTII))=,");
+    puts("         :=;)i=:,,    ,i++::i))I)ITTTTTTTTTTIIII)=+'");
+    puts("       .+ii)i=::,,   ,,::=i)))iIITTTTTTTTIIIII)=+");
+    puts("      ,==)ii=;:,,,,:::=ii)i)iIIIITIIITIIII))i+:'");
+    puts("     +=:))i==;:::;=iii)+)=  `:i)))IIIII)ii+'");
+    puts("   .+=:))iiiiiiii)))+ii;");
+    puts("  .+=;))iiiiii)));ii+");
+    puts(" .+=i:)))))))=+ii+");
+    puts(".;==i+::::=)i=;");
+    puts(",+==iiiiii+,");
+    puts("`+=+++;`");
+}
+
+void dibujoLuna () {
+
+    puts("                     .--------------.");
+    puts("                .---'  o        .    `---.");
+    puts("             .-'    .    O  .         .   `-.");
+    puts("          .-'     @@@@@@       .             `-.");
+    puts("        .'@@   @@@@@@@@@@@       @@@@@@@   .    `.");
+    puts("      .'@@@  @@@@@@@@@@@@@@     @@@@@@@@@         `.");
+    puts("     /@@@  o @@@@@@@@@@@@@@     @@@@@@@@@     O     \\ ");
+    puts("    /        @@@@@@@@@@@@@@  @   @@@@@@@@@ @@     .  \\ ");
+    puts("   /@  o      @@@@@@@@@@@   .  @@  @@@@@@@@@@@     @@ \\ ");
+    puts("  /@@@      .   @@@@@@ o       @  @@@@@@@@@@@@@ o @@@@ \\ ");
+    puts(" /@@@@@                  @ .      @@@@@@@@@@@@@@  @@@@@ \\ ");
+    puts(" |@@@@@    O    `.-./  .        .  @@@@@@@@@@@@@   @@@  |\ ");
+    puts(" / @@@@@        --`-'       o        @@@@@@@@@@@ @@@    . \ ");
+    puts(" |@ @@@@ .  @  @    `    @            @@      . @@@@@@   | ");
+    puts(" |   @@                         o    @@   .     @@@@@@   | ");
+    puts(" |  .     @   @ @       o              @@   o   @@@@@@.  | ");
+    puts(" \\     @    @       @       .-.       @@@@       @@@    / ");
+    puts(" |  @    @  @              `-'     . @@@@     .    .    |" );
+    puts(" \\ .  o       @  @@@@  .              @@  .           . /");
+    puts("  \\      @@@    @@@@@@       .                   o     /");
+    puts("   \\    @@@@@   @@\@@    /        O          .        /");
+    puts("    \\ o  @@@       \\ \\  /  __        .   .     .--.  /");
+    puts("     \\      .     . \\.-.---                   `--'  /");
+    puts("      `.             `-'      .                   .'");
+    puts("        `.    o     / | `           O     .     .'");
+    puts("          `-.      /  |        o             .-'");
+    puts("             `-.          .         .     .-'");
+    puts("                `---.        .       .---'");
+    puts("                     `--------------'");
+}
+
+void dibujoOrbitaTerrestre () {
+    puts(" .      .      .      .      .      .      .      .      .      .      .");
+    puts(".                               .       .       .       .       .       .");
+    puts("   .        .        .        .        .        .        .        .        .");
+    puts("     .         .         .        _......____._        .         .");
+    puts("   .          .          . ..--'"" .           ----------...          .");
+    puts("                   _...--""        ................       `-.              .");
+    puts("                .-'        ...:'::::;:::%:.::::::_;;:...     `-.");
+    puts("             .-'       ..::::'''''   _...---'"""":::+;_::.      `.      .");
+    puts("  .        .' .    ..::::'      _.-""               :::)::.       `.");
+    puts("         .      ..;:::'     _.-'         .             f::'::    o  _");
+    puts("        /     .:::%'  .  .-                         .-.  ::;;:.   /---x");
+    puts("  .   .'     ::.::'    .-     _.-------.           (   )  ::.::  |_.-' |");
+    puts("     .'    ::;:'    .'     .-  .d@@b.   \    .    . `-'   ::%::   \\_ _/    .");
+    puts("    .'    :,::'    /   . _'    8@@@@8   j      .-'       :::::       o");
+    puts("    | .  :.%:' .  j     (_)    `@@@P'  .'   .-          ::.::    .  f");
+    puts("    |    ::::     (        -..____...-'  .-           .::::'       /");
+    puts(".   |    `:`::    `.                ..--'        .  .::'::   .    /");
+    puts("    j     `:::::    `-._____...---""             .::%:::'       .'  .");
+    puts("     \\      ::.:%..             .       .    ...:,::::'       .'");
+    puts(" .    \\       `:::`:..                ....::::.::::'       .-'          .");
+    puts("       \\    .   ``:::%::`::.......:::::%::.::::''       .-'");
+    puts("      . `.        . ``::::::%::::.::;;:::::'''      _.-'          .");
+    puts("  .       `-..     .    .   ````'''''         . _.-'     .          .");
+    puts("         .    ""--...____    .   ______......--' .         .         .");
+    puts("  .        .        .    """"""""     .        .        .        .        .");
+    puts(" .       .       .       .       .       .       .       .       .");
+    puts("     .      .      .      .      .      .      .      .      .      .      .");
+}
+
+void dibujoEEI () {
+
+    puts("                /\\");
+    puts("                ||");
+    puts("               ====");
+    puts("               |  |");
+    puts("               |  |");
+    puts("               ====");
+    puts("               XXXX");
+    puts("               |\\/|");
+    puts("               |/\|");
+    puts("               |\\/|");
+    puts("               |/\|");
+    puts("               |\\/|");
+    puts("               |/\\|");
+    puts("              /____\\");
+    puts("              |    |");
+    puts("              |    |");
+    puts("             /      \\");
+    puts("            /        \\");
+    puts("           /          \\");
+    puts("          /            \\");
+    puts("         /              \\");
+    puts("         ----------------");
+    puts("         |--------------|");
+    puts("         |              |");
+    puts("         |     ______   |");
+    puts("         |     |        |");
+    puts("         |     |---     |");
+    puts("         |     |_____   |");
+    puts("         |     ______   |");
+    puts("         |     |        |");
+    puts("         |     |---     |");
+    puts("         |     |_____   |");
+    puts("         |              | ");
+    puts("         |     -----    |");
+    puts("         |       |      |");
+    puts("         |       |      |");
+    puts("         |       |      |");
+    puts("         |     -----    |");
+    puts("         |              |");
+    puts("         |      __      |");
+    puts("        /|      ||      |\\");
+    puts("       / |      ||      | \\");
+    puts("      /  |      ||      |  \\");
+    puts("     /   |      ||      |   \\");
+    puts("-----    |      HH      |    -----");
+    puts("|   |    |      HH      |    |   |");
+    puts("|   |    |      HH      |    |   |");
+    puts("|   |    |      HH      |    |   |");
+    puts("|   |    |______HH______|    |   |");
+    puts("--------/       HH       \\--------");
+}
