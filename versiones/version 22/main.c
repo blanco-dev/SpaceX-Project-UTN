8c9289481e24b1b09b0c2ebec69f987f3a27c076
Fabrizio Golisciano <72052696+Fabri-dev@users.noreply.github.com>
2023-06-20 22:39:08 -0300

    Add files via upload

---
+++ versiones/version 22/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "astronauta.h"
+#include "naves.h"
+#include "menu.h"
+#include "misiones.h"
+#include "windows.h"
+#define DIM_MAX_ARCHI 15
+#define DIM_MAX_STR 20
+
+
+int main()
+{
+    char archivoAstronautas[DIM_MAX_ARCHI] = "astronautas";
+    char archivoNaves[DIM_MAX_ARCHI]="naves";
+    char archivoMisiones[DIM_MAX_ARCHI]="misiones";
+
+    Sleep(3000);
+    //menuPrincipal(archivoAstronautas,archivoNaves,archivoMisiones);
+
+    //lanzarUnaMision(archivoAstronautas,archivoMisiones,archivoNaves);
+    //cargarArchivoNaves(archivoNaves);
+    //mostrarTodasLasNaves(archivoNaves);
+    //ConsultaNave(archivoNaves);
+    //stMision aux;
+    //aux=crearUnaMision(archivoMisiones,archivoNaves,archivoAstronautas);
+    //cargarArchivoMisiones(archivoAstronautas,archivoNaves,archivoMisiones);
+    //mostrarTodasLasMisiones(archivoMisiones,archivoNaves);
+
+    return 0;
+}
