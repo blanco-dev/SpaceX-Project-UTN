8ce4e55119deafe31f21ba9820f19281a3ee6652
Fabrizio Golisciano <72052696+Fabri-dev@users.noreply.github.com>
2023-06-22 00:42:31 -0300

    Add files via upload

---
+++ versiones/version 18/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "astronauta.h"
+#include "naves.h"
+#include "menu.h"
+#define DIM_MAX_ARCHI 15
+#define DIM_MAX_STR 20
+
+int main()
+{
+    char archivoAstronautas[DIM_MAX_ARCHI] = "astronautas";
+    char archivoNaves[DIM_MAX_ARCHI]="naves";
+    char archivoMisiones[DIM_MAX_ARCHI]="misiones";
+    //menuPrincipal(archivoAstronautas,archivoNaves,archivoMisiones);
+
+    //cargarArchivoNaves(archivoNaves);
+    //mostrarTodasLasNaves(archivoNaves);
+    //ConsultaNave(archivoNaves);
+
+int dato;
+
+
+
+    scanf("%i",dato);
+
+//printf("dato: \n",dato);
+dato = validarCaracteresEnEntero(dato);
+printf("ingrese un numero: %i\n",dato);
+
+
+    return 0;
+}
