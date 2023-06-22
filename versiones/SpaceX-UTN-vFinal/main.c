13de3e84bac001f4a049049fd783b13d43172cb0
leocaimmi <128081908+leocaimmi@users.noreply.github.com>
2023-06-21 23:33:09 -0300

    Add files via upload

---
+++ versiones/SpaceX-UTN-vFinal/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "astronauta.h"
+#include "nave.h"
+#include "menu.h"
+#include "misiones.h"
+#include "windows.h"
+#define DIM_MAX_ARCHI 15
+#define DIM_MAX_STR 20
+
+
+int main()
+{
+    srand(time(NULL));
+
+    char archivoAstronautas[DIM_MAX_ARCHI] = "astronautas";
+    char archivoNaves[DIM_MAX_ARCHI]="naves";
+    char archivoMisiones[DIM_MAX_ARCHI]="misiones";
+
+
+    menuPrincipal(archivoAstronautas,archivoNaves,archivoMisiones);
+
+    return 0;
+}
